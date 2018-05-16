d5f4cc5e4bf2d7a57b7f58fafb23d75f93e64d9b
Nico Valencia <nico@tsukiyomi.local>
2018-05-16 12:55:01 -0600

    Replaced redux-logger with redux-dev-tools.

--- src/Store.js
+++ src/Store.js
@@ -1,5 +1,6 @@
 import thunkMiddleware from 'redux-thunk'
 import { createStore, applyMiddleware, combineReducers } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 import listings from './reducers/Listings'
 import profile from './reducers/Profile'
@@ -9,11 +10,6 @@ import users from './reducers/Users'
 
 let middlewares = [thunkMiddleware]
 
-if (process.env.NODE_ENV !== 'production') {
-  const { logger } = require(`redux-logger`)
-  middlewares.push(logger)
-}
-
 const store = createStore(
   combineReducers({
     listings,
@@ -22,7 +18,9 @@ const store = createStore(
     alert,
     users,
   }),
-  applyMiddleware(...middlewares)
+  composeWithDevTools(
+    applyMiddleware(...middlewares)
+  )
 )
 
 export default store
